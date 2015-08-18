6515fd2e995d3b8a15b1f011f2439bc08aca438e
Greg Kristofor <grkristofor@gmail.com>
2015-08-17 21:59:30 -0400

    Intial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+for (var i = 99; i > -1 ; i--){
+	console.log(i + " " + "bottles of root beer on the wall" + " " + i + " " + "bottles of root beer...take one down, pass it around" + " " + i + " " + "bottles of root beer on the wall");
+};
+
+
+for (var i = 1; i <100; i++){
+	console.log(i);
+	if (i % 3===0){
+		console.log("fizz");{
+		if (i % 5===0)
+		console.log("bizz");{
+			if (i % 15===0)
+			console.log("fizzbizz");
+			}
+		}	
+	}
+}
+
+
+// 	console.log(i);{
+// 		if (i % 3 === 0){
+// 			console.log("fizz");{
+// 			// 	if (i % 5);
+// 			// console.log("bizz");
+// 			}
+// 		}
+// 	}
+// }
+
+
